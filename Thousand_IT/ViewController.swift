27eb619f7100603428bf239ce3d0f0182c9e08d0
Shyngys <Shyngys>
2020-11-15 17:17:44 +0600

    Initial Commit

---
+++ Thousand_IT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thousand_IT
+//
+//  Created by Шынгыс on 11/15/20.
+//  Copyright © 2020 Шынгыс. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
